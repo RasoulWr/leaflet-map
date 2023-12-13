d03a1f28a2d0f27783c7526e05dd50b80514546e
RasoulWr <rasuol.gholami1377@gmail.com>
2023-12-13 12:01:31 +0330

    get-User-Live-Location

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 document.getElementById('map').style.setProperty('height',window.innerHeight + 'px');
 
 // set new location
-map.setView([40.7590322,-74.051],15);//newyork manhatan
+// map.setView([40.7590322,-74.051],15);//newyork manhatan
 
 
 // marker and pin
@@ -39,18 +39,77 @@ L.marker([35.7581166,51.3640]).addTo(map).bindPopup("رستوران داش نص�
 // // 3: search location in map window (we must do query in data base)
 // // 4: display loacation markers in map
 // });
-map.on("dblclick",function(event){
-    
-    L.marker([event.latlng.lat,event.latlng.lng]).addTo(map);
-    // 1: add marker in clicked position
-    // 2: open modal (form) for save the cliked position
-    // 3: fill the form and submit location for send to server
-    // 4: save location in database (status : pending review)
-    // 5: review location and verify if ok 
-})
 
-setTimeout(() => {
-   
-}, 2000);
+
+// map.on("dblclick",function(event){
+    
+//     L.marker([event.latlng.lat,event.latlng.lng]).addTo(map);
+//     // 1: add marker in clicked position
+//     // 2: open modal (form) for save the cliked position
+//     // 3: fill the form and submit location for send to server
+//     // 4: save location in database (status : pending review)
+//     // 5: review location and verify if ok 
+// })
+
+
+
+var current_position, current_accuracy;
+  map.on('locationfound', function(e) {
+    // if position defined, then remove the existing position marker and accuracy circle from the map
+    if (current_position) {
+        map.removeLayer(current_position);
+        map.removeLayer(current_accuracy);
+    }
+    var radius = e.accuracy;
+    current_position = L.marker(e.latlng).addTo(map)
+        .bindPopup("دقت تقریبی: " + radius + " متر").openPopup();
+    current_accuracy = L.circle(e.latlng, radius).addTo(map);
+});
+   map.on('locationerror', function(e) {
+      alert(e.message);
+});
+
+function locate() {
+    map.locate({ setView: true, maxZoom: defaultZoom });
+}
+
+setInterval(locate,5000);
+
+
+
+
+// map.locate({setView: true, watch: true}) /* This will return map so you can do chaining */
+//   .on('locationfound', function(e){
+//     var marker = L.marker([e.latitude, e.longitude]).bindPopup('Your are here :)');
+//     var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
+//         weight: 1,
+//         color: 'blue',
+//         fillColor: '#cacaca',
+//         fillOpacity: 0.2
+//     });
+//     map.addLayer(marker);
+//     map.addLayer(circle);
+//     // marker.addTo(map);1
+//     // circle.addTo(map);
+// })
+//   .on('locationerror', function(e){
+//     console.log(e);
+//     alert("Location access denied.");
+// });
+
+
+
+
+
+
+//L.marker([35.28119,47.419]).addTo(map).bindPopup("موقیت مکانی من");
+
+
+    
+
+
+
+
+
 
 
--- tpl/tpl_index.php
+++ tpl/tpl_index.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Map</title>
-    <link rel="stylesheet" href="assets/css/style.css" />
-    <link rel="stylesheet" href="assets/css/leaflet.css<?="?v=".rand(100,9999) ?>"/>
-    <script src="assets/js/leaflet.js" <?php echo"?v=".rand(100,9999) ?>></script>
+    <link rel="stylesheet" href="assets/css/style.css"/>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+    
+  
+    <!-- <script src="assets/js/leaflet.js" <php echo"?v=".rand(100,9999) ?>></script> -->
+
 </head>
 <body>
     <div class="main">
